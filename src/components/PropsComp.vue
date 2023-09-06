3126e1676599a19c1528a4ab77049f04aa39f357
Dreamice <dreamice13@foxmail.com>
2023-09-06 23:07:32 +0800

    Play Props

--- src/components/PropsComp.vue
+++ src/components/PropsComp.vue
@@ -1,11 +1,24 @@
+<!--
+ * @Author: Dreamice dreamice13@foxmail.com
+ * @Date: 2023-09-04 13:53:36
+ * @LastEditors: Dreamice dreamice13@foxmail.com
+ * @LastEditTime: 2023-09-05 15:43:46
+ * @FilePath: \PlayVue\src\components\PropsComp.vue
+ * @Description: 
+-->
 <script setup lang='ts'>
-    const props = defineProps(['p1'])
-
+    // const props = defineProps(['fatherMessage'])
+    const props = defineProps({
+        fatherMessage: String,
+        id: Number,
+        name: String,
+    })
 </script>
 
 <template>
     <div>
-        <span>我是子组件，父组件传递的值为： {{ props.p1 }}</span>
+        <span>我是子组件，父组件传递的值为： {{ fatherMessage }}</span>
+        <span>使用一个对象绑定多个prop, id： {{ id }}, name是 {{ name }}</span>
     </div>
 </template>
 
--- src/views/PropsView.vue
+++ src/views/PropsView.vue
@@ -2,19 +2,23 @@
  * @Author: Dreamice dreamice13@foxmail.com
  * @Date: 2023-09-04 13:44:35
  * @LastEditors: Dreamice dreamice13@foxmail.com
- * @LastEditTime: 2023-09-04 13:56:43
+ * @LastEditTime: 2023-09-05 00:39:47
  * @FilePath: \PlayVue\src\views\propsView.vue
  * @Description: 
 -->
 <script setup lang='ts'>
 import PropsComp from '@/components/PropsComp.vue';
 
+const obj = {
+    id: 1, 
+    name: "张三"
+}
 </script>
 
 <template>
     <div>
         <h1>props</h1>
-        <PropsComp p1="4"></PropsComp>
+        <PropsComp father-message="4" v-bind="obj"></PropsComp>
     </div>
 </template>
 
